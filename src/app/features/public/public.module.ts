2e24ba2e3e4319ab27473e5981492260c2a86b19
Devalfe <mdt.webmaster@gmail.com>
2023-05-14 18:08:37 -0500

    feat: add home components

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -4,12 +4,22 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RoadmapComponent } from './components/roadmap/roadmap.component';
+import { TokenomicsComponent } from './components/tokenomics/tokenomics.component';
+import { HomeComponent } from './components/Home/home.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    RoadmapComponent,
+    TokenomicsComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,

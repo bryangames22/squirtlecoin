8cd4287cc33f7e3af63fec660bd6aa7beffedac4
Devalfe <mdt.webmaster@gmail.com>
2023-06-07 23:29:13 -0500

    feat: 💚 squirtle paper

--- src/app/features/squirtle-paper/container/container.component.ts
+++ src/app/features/squirtle-paper/container/container.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'sqr-container',
-    templateUrl: './container.component.html',
+  templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent {}
